d0cc103bcbdee0b5290ad30fbae47a4bcae0ff1f
vicente-ai <valarconi@ing.ucsc.cl>
2024-11-26 21:48:46 -0300

    funcionando

--- Ventanas/Secretarios/VerHoraWindow.axaml.cs
+++ Ventanas/Secretarios/VerHoraWindow.axaml.cs
@@ -8,28 +8,21 @@ namespace ProyectoConsultorio
         public VerHoraWindow()
         {
             InitializeComponent();
-
         }
 
-        Button botonPresionado = Button;
-
-        if(botonPresionado.Name == "btnVolverMedico")
-            {
         private void VolverMenu(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
         {
             var menuWindow = new MenuWindow();
             menuWindow.Show(); // Muestra la ventana secundaria
             this.Close();
         }
-    }
-        else if(botonPresionado.Name == "btnVolverSecretario")
-        {
         private void VolverMenuMed(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
-    {
-        var medMenuWindow = new MedMenuWindow();
-        medMenuWindow.Show(); // Muestra la ventana secundaria
-        this.Close();
-    }
+        {
+            var medMenuWindow = new MedMenuWindow();
+            medMenuWindow.Show(); // Muestra la ventana secundaria
+            this.Close();
         }
     }
 }
+        
+    
